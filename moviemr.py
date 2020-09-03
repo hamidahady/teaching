b86752c7e50c64072f94e72f8232ab623b224ee9
hamidahady <hamidahady@yahoo.com>
2020-09-03 15:43:34 -0400

    movie

---
+++ moviemr.py
@@ -0,0 +1,19 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class RatingHistogram(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_rating,
+                   reducer=self.reducer_rating)
+        ]
+
+    def mapper_rating(self, _, line):
+        (userid, movieid, rating, timestamp) = line.split('\t')
+        yield movieid, 1
+
+    def reducer_rating(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    RatingHistogram.run()
